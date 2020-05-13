0ebe3df00f8470a9ce2bc6c97daaaf4b27e981d7
giphilgolden <62906944+giphilgolden@users.noreply.github.com>
2020-05-12 20:01:15 -0400

    finished show button

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,8 @@ let rollButton = document.querySelector("#roll-button")
 let rollNumberInput = document.querySelector("#roll-number")
 let rollSumTotal = document.querySelector("#roll-sum")
 let dieRolls = []
+let rollResults = document.querySelector("#roll-results")
+let rollShow = document.querySelector("#roll-show")
 
 rollButton.addEventListener("click", function(){
     
@@ -13,22 +15,51 @@ rollButton.addEventListener("click", function(){
     while (numberOfRolls >= index ) {
         if (Math.random() * 6 < 1) {
         console.log("6")
+        dieRolls.push("6")
     } else if (Math.random() * 5 < 1) {
         console.log("5")
+        dieRolls.push("5")
     } else if (Math.random() * 4 < 1) {
         console.log("4")
+        dieRolls.push("4")
     } else if (Math.random() * 3 < 1) {
         console.log("3")
+        dieRolls.push("3")
     } else if (Math.random() * 2 < 1) {
         console.log("2")
+        dieRolls.push("2")
     } else {
         console.log("1")
+        dieRolls.push("1")
+        
     }  
      index += 1
     }
+    console.log(dieRolls)
+    console.log("dieRolls length is" + " " + dieRolls.length)
 
-  console.log(dieRolls)
+    let i = 0
+    while (dieRolls.length > i) {
+        
+        rollResults.innerHTML += ("<li>" + " " + dieRolls[i] + " " + "</li>")
+        let baseRollValue = 0
+        let newRollValue = baseRollValue += dieRolls[1]
+        let newestRollValue = (newRollValue += dieRolls[i])
+        rollSumTotal.innerHTML = (newRollValue)
+        console.log("newRollValue is " + newRollValue)
+        console.log(newestRollValue)
+        i += 1
+        console.log("i = " +i)
+        baseRollValue += dieRolls[i]
+    }
 
-    
 
+})
+
+rollShow.addEventListener("click", function() {
+    if (rollResults.style.display === "inline-block") {
+        rollResults.style.display = "none";
+      } else {
+        rollResults.style.display = "inline-block";
+      }
 })
